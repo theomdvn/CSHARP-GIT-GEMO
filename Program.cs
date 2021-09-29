28228e33038222eaca1578c8e5f5ecfa31e9e89c
theomdvn <theomida@gmail.com>
2021-09-29 16:57:29 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSHARP_GIT_DEMO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
